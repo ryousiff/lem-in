15f38e9da7f553a762176255299929238493986c
fmahmood <roro2004yousif@gmail.com>
2024-05-28 22:51:23 +0300

    yay

--- functions/structs.go
+++ functions/structs.go
@@ -1,41 +1,42 @@
 package lem
 
 type Room struct {
-    Name     string
-    CoordX   string
-    CoordY   string
-    Visited  bool
-    IsEnd    bool
-    IsStart  bool
-    Links    []*Link
-    Neighbors []*Room
-    LineNum  int // Added LineNum field
+	Name      string
+	CoordX    string
+	CoordY    string
+	Visited   bool
+	IsEnd     bool
+	IsStart   bool
+	Links     []*Link
+	Neighbors []*Room
+	LineNum   int // Added LineNum field
 }
 
 type Ant struct {
-    ID int
+	ID int
 }
 
 type Farm struct {
-    Rooms        []*Room
-    Ants         []*Ant
-    Links        []*Link
-    NumAnt       int
-    StartRoom    *Room
-    EndRoom      *Room
-    AntPositions map[int]*Room
-    Paths        []*Path
-    StartRoomLine int
-    EndRoomLine   int
+	Rooms         []*Room
+	Ants          []*Ant
+	Links         []*Link
+	NumAnt        int
+	StartRoom     *Room
+	EndRoom       *Room
+	AntPositions  map[int]*Room
+	Paths         []*Path
+	StartRoomLine int
+	EndRoomLine   int
 }
 
 type Link struct {
-    Room1 *Room
-    Room2 *Room
+	Room1 *Room
+	Room2 *Room
 }
 
 type Path struct {
-    Rooms []*Room
+	Rooms []*Room
 }
 
 //paths must be same number as links from the start
+//filter the shortest path
